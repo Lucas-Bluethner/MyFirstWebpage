0ca3c2e8cadac487f6dd357ca6a3dc4fac85d92f
Lucas-Bluethner <lucas.bluethner@mail.mcgill.ca>
2018-12-07 10:38:41 -0500

    My first webpage completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,23 @@
 var myImage = document.querySelector('img');
 
+var myHTML = document.querySelector('html');
+
+var myHeading = document.querySelector('h1');
+
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute ('src','images/firefox2.png');
+        myImage.setAttribute ('src','images/firefox2.png');
+        myHTML.style.backgroundColor = "#ff7700"
+        myHeading.style.color = "#ff7700"
     } else {
-      myImage.setAttribute ('src','images/firefox-icon.png');
+        myImage.setAttribute ('src','images/firefox-icon.png');
+        myHTML.style.backgroundColor = "#9900ff"
+        myHeading.style.color = "#9900ff"
     }
 }
 
 var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
 
 function setUserName() {
   var myName = prompt('Please enter your name.');
